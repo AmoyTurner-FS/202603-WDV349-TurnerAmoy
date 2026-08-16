c00367a4e6f73b8ab8fbbc5a6322fabc91fc3b65
AmoyTurner-FS <AKTurner1@fullsail.com>
2026-08-15 23:37:41 -0400

    Updated page header titles based on current route

--- dev/portfolio/src/components/Navbar.jsx
+++ dev/portfolio/src/components/Navbar.jsx
@@ -1,13 +1,25 @@
+import { useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const location = useLocation();
+
+  const pageTitles = {
+    "/": "Dashboard",
+    "/search": "Search Cars",
+    "/add-vehicle": "Add New Vehicle",
+    "/favorites": "Favorites",
+  };
+
+  let pageTitle = pageTitles[location.pathname] || "Vehicle Details";
+
   return (
     <header className="navbar">
-      <h2>Dashboard</h2>
+      <h1>{pageTitle}</h1>
 
-      <div className="navbar-right">
-        <div className="avatar"></div>
-        <button className="menu-button">☰</button>
+      <div className="navbar-actions">
+        <div className="profile-circle"></div>
+        <span className="menu-icon">☰</span>
       </div>
     </header>
   );
