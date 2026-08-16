f5c9cd406a7d18340411f3ae4ed8d7278fbdcec8
AmoyTurner-FS <AKTurner1@fullsail.com>
2026-08-15 22:27:54 -0400

    Built the footer component

--- dev/portfolio/src/components/AppLayout.jsx
+++ dev/portfolio/src/components/AppLayout.jsx
@@ -1,17 +1,22 @@
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 import "./AppLayout.css";
 
 function AppLayout({ children }) {
   return (
-    <div className="app-layout">
-      <Sidebar />
+    <div className="app-wrapper">
+      <div className="app-layout">
+        <Sidebar />
 
-      <div className="content-area">
-        <Navbar />
+        <div className="content-area">
+          <Navbar />
 
-        <main className="main-content">{children}</main>
+          <main className="main-content">{children}</main>
+        </div>
       </div>
+
+      <Footer />
     </div>
   );
 }
