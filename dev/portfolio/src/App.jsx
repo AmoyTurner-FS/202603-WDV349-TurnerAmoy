85fb17a7967e8aa3b3613e70de84121715e68c0a
AmoyTurner-FS <AKTurner1@fullsail.com>
2026-08-15 20:58:42 -0400

    Configure React routing

--- dev/portfolio/src/App.jsx
+++ dev/portfolio/src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
-import SearchBar from "./components/SearchBar";
+import Dashboard from "./pages/Dashboard";
+import SearchCars from "./pages/SearchCars";
+import AddVehicle from "./pages/AddVehicle";
+import Favorites from "./pages/Favorites";
+import VehicleDetails from "./pages/VehicleDetails";
 
 function App() {
   return (
-    <AppLayout>
-      <SearchBar />
-    </AppLayout>
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/search" element={<SearchCars />} />
+          <Route path="/add-vehicle" element={<AddVehicle />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/vehicle/:id" element={<VehicleDetails />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   );
 }
 
