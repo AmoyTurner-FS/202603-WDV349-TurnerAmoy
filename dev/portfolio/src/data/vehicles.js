e664c738614182dc3d6d6ff9699c4e58003b44c5
AmoyTurner-FS <AKTurner1@fullsail.com>
2026-08-22 23:05:30 -0400

    Implement vehicle search functionality

---
+++ dev/portfolio/src/data/vehicles.js
@@ -0,0 +1,52 @@
+const vehicles = [
+  {
+    id: "1",
+    year: "2022",
+    make: "Honda",
+    model: "Civic",
+    price: "24500",
+    mileage: "28000",
+  },
+  {
+    id: "2",
+    year: "2021",
+    make: "Toyota",
+    model: "Camry",
+    price: "23000",
+    mileage: "32000",
+  },
+  {
+    id: "3",
+    year: "2023",
+    make: "Nissan",
+    model: "Altima",
+    price: "27000",
+    mileage: "18000",
+  },
+  {
+    id: "4",
+    year: "2020",
+    make: "BMW",
+    model: "330i",
+    price: "29500",
+    mileage: "41000",
+  },
+  {
+    id: "5",
+    year: "2022",
+    make: "Audi",
+    model: "A4",
+    price: "31000",
+    mileage: "26000",
+  },
+  {
+    id: "6",
+    year: "2021",
+    make: "Lexus",
+    model: "IS 300",
+    price: "32500",
+    mileage: "30000",
+  },
+];
+
+export default vehicles;
